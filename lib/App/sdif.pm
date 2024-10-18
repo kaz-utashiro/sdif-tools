1c0954fb7a1c3e9e622779fbe4d825a5113c13f8
Kazumasa Utashiro <kaz@utashiro.com>
2024-10-18 14:45:28 +0900

    Checking in changes prior to tagging of version 4.36.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 578f0a8..8f1d917 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for Perl application 'sdif-tools'.
     
     {{$NEXT}}
     
    +4.36 2024-10-18T05:45:19Z
    +
    +    - improve mecab algorithm to split non-word sequence
    +
     4.35 2024-10-06T07:04:40Z
     
         - sdif supports #ifdef and custom container format

--- lib/App/sdif.pm
+++ lib/App/sdif.pm
@@ -7,7 +7,7 @@ our @EXPORT      = qw(usage);
 our %EXPORT_TAGS = ();
 our @EXPORT_OK   = qw();
 
-our $VERSION = "4.35";
+our $VERSION = "4.36";
 
 use Pod::Usage;
 
--- lib/App/watchdiff.pm
+++ lib/App/watchdiff.pm
@@ -232,7 +232,7 @@ watchdiff - repeat command and watch differences
 
 =head1 VERSION
 
-Version 4.35
+Version 4.36
 
 =head1 DESCRIPTION
 
