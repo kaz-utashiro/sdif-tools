d59efc8fe41163850d33c92c0bb682de0cf57f89
Kazumasa Utashiro <kaz@utashiro.com>
2024-10-30 19:34:42 +0900

    Checking in changes prior to tagging of version 4.38.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index d87ef74..6a7e9cb 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,11 @@ Revision history for Perl application 'sdif-tools'.
     
     {{$NEXT}}
     
    +4.38 2024-10-30T10:34:35Z
    +
    +    - print filename from conflict-marker diff input
    +    - update --no-command behavior and introduce --no-filename option
    +
     4.37 2024-10-19T02:33:20Z
     
         - cdif does not \p{Punct} sequence as a word

--- lib/App/sdif.pm
+++ lib/App/sdif.pm
@@ -7,7 +7,7 @@ our @EXPORT      = qw(usage);
 our %EXPORT_TAGS = ();
 our @EXPORT_OK   = qw();
 
-our $VERSION = "4.37";
+our $VERSION = "4.38";
 
 use Pod::Usage;
 
--- lib/App/watchdiff.pm
+++ lib/App/watchdiff.pm
@@ -232,7 +232,7 @@ watchdiff - repeat command and watch differences
 
 =head1 VERSION
 
-Version 4.37
+Version 4.38
 
 =head1 DESCRIPTION
 
