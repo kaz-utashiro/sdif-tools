d0f9ff0e6377b037dbeb9ba7657d1d8371b6d824
Kazumasa Utashiro <kaz@utashiro.com>
2025-02-13 15:15:28 +0900

    Checking in changes prior to tagging of version 4.41.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index fb76fac..c471829 100644
    --- a/Changes
    +++ b/Changes
    @@ -2,6 +2,10 @@ Revision history for Perl application 'sdif-tools'.
     
     {{$NEXT}}
     
    +4.41 2025-02-13T06:15:20Z
    +
    +    - improve handling CRNL ending text
    +
     4.40 2025-01-29T06:52:04Z
     
         - introduce "cdif --lxl --style=diff" option

--- lib/App/sdif.pm
+++ lib/App/sdif.pm
@@ -7,7 +7,7 @@ our @EXPORT      = qw(usage);
 our %EXPORT_TAGS = ();
 our @EXPORT_OK   = qw();
 
-our $VERSION = "4.40";
+our $VERSION = "4.41";
 
 use Pod::Usage;
 
--- lib/App/watchdiff.pm
+++ lib/App/watchdiff.pm
@@ -232,7 +232,7 @@ watchdiff - repeat command and watch differences
 
 =head1 VERSION
 
-Version 4.40
+Version 4.41
 
 =head1 DESCRIPTION
 
